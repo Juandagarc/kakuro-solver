5d31ce3b443343e939e3b5297819f46da49d1ce8
Juandagarc <juandagarce@gmail.com>
2024-05-24 16:40:59 -0500

    is consistence

--- kakuro.py
+++ kakuro.py
@@ -1,68 +1,115 @@
-
-import copy
-import itertools
 from colorama import Fore, Style
 
 class CSP:
     def __init__(self):
         self.vars = {}
         self.constraints ={'Dif':[], 'SameDomain2': [], 'SameDomain3': [], 'NotRepeated': []}
+        self.board = []
 
     def Vars_Doms(self):
         rows = set(range(1, 10))
         cols = 'ABCDEFGHI'
-        self.Vars = {f"{c}{r}": rows.copy() for c in cols for r in rows}
+        self.vars = {f"{c}{r}": rows.copy() for c in cols for r in rows}
 
     def assign(self, var, value, assignment):
         assignment[var] = value
 
     def initBoard(self, nameFile):
-      with open(nameFile, 'r') as file:
-        lines = file.readlines()
-      
-      board = []
-      for line in lines:
-        row = line.strip().split(' ')
-        # Se separan los objetos internos que tengan \ como separador
-        for i in range(len(row)):
-          if '\\' in row[i]:
-            row[i] = row[i].split('\\')
-        board.append(row)
-      
-      return board
-    
-    # Restricciones de las filas.
+        with open(nameFile, 'r') as file:
+            lines = file.readlines()
+        
+        self.board = []
+        for line in lines:
+            row = line.strip().split(' ')
+            # Se separan los objetos internos que tengan \ como separador
+            row_values = []
+            for value in row:
+                if '\\' in value:
+                    sub_values = value.split('\\')
+                    row_values.append(sub_values)
+                else:
+                    row_values.append(value)
+            self.board.append(row_values)
+        
+        return self.board
 
-    def printBoard(self, board):
-      colsIndex = "ABCDEFGHI" # Columnas
-      numbers_color = Fore.GREEN
-      letters_color = Fore.BLUE
-      region_color = Fore.RED
-      separator = "\t" + region_color + ("+" + "-" * 15 ) * 10 + "+" + Style.RESET_ALL
+    def printDomains(self):
+        for var, domain in self.vars.items():
+            print(f"{var}: {domain}")
 
-      print("KAKURO SOLVER:")
-      print(separator)
-      print("\t|\t/\t|\t" + letters_color + "A" + Style.RESET_ALL + "\t|\t" + letters_color + "B" + Style.RESET_ALL + "\t|\t" + letters_color + "C" + Style.RESET_ALL + "\t|\t" + letters_color + "D" + Style.RESET_ALL + "\t|\t" + letters_color + "E" + Style.RESET_ALL + "\t|\t" + letters_color + "F" + Style.RESET_ALL + "\t|\t" + letters_color + "G" + Style.RESET_ALL + "\t|\t" + letters_color + "H" + Style.RESET_ALL + "\t|\t" + letters_color + "I" + Style.RESET_ALL + "\t|")
-      print(separator)
-      for i in range(9):
-        if i % 3 == 0 and i != 0:
-          print(separator)
-        row_values = []
-        for j in range(9):
-          cell_value = board[i][j]
-          if isinstance(cell_value, list):
-            row_values.append(Fore.BLACK + "\\".join(map(str, cell_value)) + Style.RESET_ALL)
-          elif 'N' in str(cell_value):
-            row_values.append(Fore.BLACK + str(cell_value) + Style.RESET_ALL)
-          else:
-            row_values.append(numbers_color + str(cell_value) + Style.RESET_ALL)
-        print("\t|\t" + letters_color + str(i+1) + Style.RESET_ALL + "\t|\t" + "\t|\t".join(row_values) + "\t|")
-      print(separator)
+    def printBoard(self):
+        colsIndex = "ABCDEFGHI" # Columnas
+        numbers_color = Fore.GREEN
+        letters_color = Fore.BLUE
+        region_color = Fore.RED
+        separator = "\t" + region_color + ("+" + "-" * 15 ) * 10 + "+" + Style.RESET_ALL
 
+        print("KAKURO SOLVER:")
+        print(separator)
+        print("\t|\t/\t|\t" + letters_color + "A" + Style.RESET_ALL + "\t|\t" + letters_color + "B" + Style.RESET_ALL + "\t|\t" + letters_color + "C" + Style.RESET_ALL + "\t|\t" + letters_color + "D" + Style.RESET_ALL + "\t|\t" + letters_color + "E" + Style.RESET_ALL + "\t|\t" + letters_color + "F" + Style.RESET_ALL + "\t|\t" + letters_color + "G" + Style.RESET_ALL + "\t|\t" + letters_color + "H" + Style.RESET_ALL + "\t|\t" + letters_color + "I" + Style.RESET_ALL + "\t|")
+        print(separator)
+        for i in range(9):
+            if i % 3 == 0 and i != 0:
+                print(separator)
+            row_values = []
+            for j in range(9):
+                cell_value = self.board[i][j]
+                if isinstance(cell_value, list):
+                    row_values.append(Fore.BLACK + "\\".join(map(str, cell_value)) + Style.RESET_ALL)
+                elif 'N' in str(cell_value):
+                    row_values.append(Fore.BLACK + str(cell_value) + Style.RESET_ALL)
+                else:
+                    row_values.append(numbers_color + str(cell_value) + Style.RESET_ALL)
+            print("\t|\t" + letters_color + str(i+1) + Style.RESET_ALL + "\t|\t" + "\t|\t".join(row_values) + "\t|")
+        print(separator)
 
+    def solve(self):
+        assignment = {}
+        return self.backtrack(assignment)
+
+    def backtrack(self, assignment):
+        if self.is_complete(assignment):
+            return assignment
+
+        var = self.select_unassigned_variable(assignment)
+
+        for value in self.order_domain_values(var, assignment):
+            if self.is_consistent(var, value, assignment):
+                self.assign(var, value, assignment)
+                result = self.backtrack(assignment)
+                if result:
+                    return result
+                assignment[var] = None  # Backtrack
+        return None
+
+    def is_complete(self, assignment):
+        return all(value is not None for value in assignment.values())
+
+    def select_unassigned_variable(self, assignment):
+        for var, value in assignment.items():
+            if value is None:
+                return var
+        return None
+
+    def order_domain_values(self, var, assignment):
+        return self.vars[var]
+
+    def is_consistent(self, var, value, assignment):
+        for constraint in self.constraints['Dif']:
+            if var in constraint:
+                other = constraint[0] if var == constraint[1] else constraint[1]
+                if other in assignment and assignment[other] == value:
+                    return False
+        return True
 
 #run
 csp = CSP()
 csp.Vars_Doms()
-board = csp.initBoard('solve.txt')
-csp.printBoard(board)
+csp.initBoard('solve.txt')
+csp.printBoard()
+solution = csp.solve()
+if solution:
+    print("Solution:")
+    csp.printBoard()
+else:
+    print("No solution found.")
